2e344187b84ac584099925910b153a2e6d36f1c5
Ryan Collins <ryan.collins@ksc.keene.edu>
2019-03-27 17:20:59 -0400

    Added smooth scrolling

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,33 +15,42 @@ for(var i = 0; i < links.length; i++){
     linkTags.push(<li><a href="#">{links[i]}</a></li>);
 };
 
-const Header = (props) => (
-    <div id="header">
-        <img id="logo" alt="logo" src={require("../resources/TempHeader.PNG")} />
-        <Nav fill justify variant="tabs"
-            activeKey="#"
-            onSelect={selectedKey => {
-                    window.scrollTo(0, props.scrollRefs[selectedKey].current.offsetTop);
-                    console.log(selectedKey);
-                }
-            }>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Meetings</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Gallery</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="projects">Projects</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-4">Contact Us</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="resources">Resources</Nav.Link>
-            </Nav.Item>
-        </Nav>
-    </div>
-);
+class Header extends React.Component{
+    goToTop = (e) => {
+        e.preventDefault();
+        window.scrollTo(0, 0);
+    }
+
+    render(){
+        return (
+            <div id="header">
+                <a onClick={this.goToTop} href=""><img id="logo" alt="logo" src={require("../resources/TempHeader.PNG")} /></a>
+                <Nav fill justify variant="tabs"
+                    activeKey="#"
+                    onSelect={selectedKey => {
+                            window.scrollTo(0, this.props.scrollRefs[selectedKey].current.offsetTop - 80);
+                            console.log(selectedKey);
+                        }
+                    }>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">Meetings</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">Gallery</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="projects">Projects</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-4">Contact Us</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="resources">Resources</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
+        );
+    }
+}
 
 export default Header;
