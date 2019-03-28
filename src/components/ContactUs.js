ec70dbe805d1bd713d6b71c0368fb174ec34f1a3
Ryan Collins <ryan.collins@ksc.keene.edu>
2019-03-28 16:14:04 -0400

    Added code stubs for all sections

---
+++ src/components/ContactUs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactUs extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ContactUs;
---
+++ src/components/MeetingSummary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MeetingSummary extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MeetingSummary;
