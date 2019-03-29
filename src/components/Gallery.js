a6c23b38488940a5d48f876485c01f5023761521
Ryan Collins <ryan.collins@ksc.keene.edu>
2019-03-29 13:47:18 -0400

    Updated gallery with filler banners

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,41 +7,27 @@ class Gallery extends Component {
     render() {
         return (
             <div id="banner">
-                <Carousel>
+                <Carousel indicators={false}>
                     <Carousel.Item>
                         <img
-                        className="d-block w-100"
-                        src="https://www.keene.edu/ksc/assets/files/31765/ww_computersciencerobotics_22180418.standard-hero.1600x667.jpg?20180420150402"
-                        alt="First slide"
+                            className="d-block w-100"
+                            src="https://www.keene.edu/ksc/assets/files/31765/ww_computersciencerobotics_22180418.standard-hero.1600x667.jpg?20180420150402"
+                            alt="First slide"
                         />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
-                        className="d-block w-100"
-                        src={require("../resources/Octocat.png")}
-                        alt="Third slide"
+                            className="d-block w-100"
+                            src="https://www.keene.edu/ksc/assets/files/29690/ww_karenstanish_250417_005.standard-hero.1600x667.jpg?20180330150310"
+                            alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
-                        className="d-block w-100"
-                        src={require("../resources/Octocat.png")}
-                        alt="Third slide"
+                            className="d-block w-100"
+                            src="https://www.keene.edu/ksc/assets/files/31499/ww_anthonyscioliinbre_02112016_07.standard-hero.1600x667.jpg?20180409100400"
+                            alt="Third slide"
                         />
-
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </div>
