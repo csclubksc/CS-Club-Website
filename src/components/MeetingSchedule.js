93e51773772dd4acf90b232bdd6db1de6fff9b3b
Ryan <ryan.collins@ksc.keene.edu>
2019-03-30 13:00:23 -0400

    Implemented core of upcoming events

--- src/components/MeetingSchedule.js
+++ src/components/MeetingSchedule.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import MeetingSummary from './MeetingSummary';
 
 class MeetingSchedule extends Component {
     render() {
         return (
-            <div ref={this.props.scrollRef}>
+            <div id="meetingSched" ref={this.props.scrollRef}>
                 <Card>
-                    <Card.Header>Meeting Schedule</Card.Header>
+                    <Card.Header>Upcoming Events</Card.Header>
                     <Card.Body>
-                        <Card.Text>Suh</Card.Text>
+                        <CardDeck id="meetingCards">
+                            <MeetingSummary title="Working with Raspberry Pis"
+                                            description="TIME"
+                                            date="April 3, 2019" /> 
+                            <MeetingSummary title="Leetcode Practice"
+                                            club_session="!"
+                                            description="TIME"
+                                            date="April 10, 2019" />
+                            <MeetingSummary title="Robocode"
+                                            description="TIME"
+                                            date="April 17, 2019" />               
+                        </CardDeck>
                     </Card.Body>
                 </Card>
             </div>
--- src/components/MeetingSummary.js
+++ src/components/MeetingSummary.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
 
 class MeetingSummary extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Card>
+                <Card.Header>{this.props.date}</Card.Header>
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Subtitle>{this.props.club_session ? "!(CS Club)" : "CS Club"}</Card.Subtitle>
+                    <Card.Text>{this.props.description}</Card.Text>
+                </Card.Body>
+            </Card>
         );
     }
 }
