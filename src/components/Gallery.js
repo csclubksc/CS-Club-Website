cc749a4fe0f194ea6ca539b45225ac1644e7cc2c
Ryan <ryan.collins@ksc.keene.edu>
2019-03-30 17:46:50 -0400

    Replaced some non-cs related filler images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,16 +14,16 @@ class Gallery extends Component {
                         />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img
+                        <img 
                             className="d-block w-100"
-                            src="https://www.keene.edu/ksc/assets/files/29690/ww_karenstanish_250417_005.standard-hero.1600x667.jpg?20180330150310"
-                            alt="Third slide"
+                            src={require("../resources/code.jpg")}
+                            alt="Second slide"
                         />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img
+                        <img 
                             className="d-block w-100"
-                            src="https://www.keene.edu/ksc/assets/files/31499/ww_anthonyscioliinbre_02112016_07.standard-hero.1600x667.jpg?20180409100400"
+                            src={require("../resources/laptopWithBook.jpg")}
                             alt="Third slide"
                         />
                     </Carousel.Item>
